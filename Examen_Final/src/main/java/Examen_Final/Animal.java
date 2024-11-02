8d7329b7fd5f1295654e58de79416a0dc492bd05
WalterRamos22 <wramosc4@miumg.edu.gt>
2024-11-02 07:53:24 -0600

    Update Animal.java

--- Examen_Final/src/main/java/Examen_Final/Animal.java
+++ Examen_Final/src/main/java/Examen_Final/Animal.java
@@ -9,5 +9,29 @@ package Examen_Final;
  * @author Walter Ramos
  */
 public class Animal {
-    
+    private int id;
+    private String nombre;
+    private String fechaNacimiento;
+    private String especie;
+
+    // Constructor
+    public Animal(int id, String nombre, String fechaNacimiento, String especie) {
+        this.id = id;
+        this.nombre = nombre;
+        this.fechaNacimiento = fechaNacimiento;
+        this.especie = especie;
+    }
+
+    // Getters y Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getFechaNacimiento() { return fechaNacimiento; }
+    public void setFechaNacimiento(String fechaNacimiento) { this.fechaNacimiento = fechaNacimiento; }
+
+    public String getEspecie() { return especie; }
+    public void setEspecie(String especie) { this.especie = especie; }
 }
