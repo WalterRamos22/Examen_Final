2d8bd398a73208fb8eedc7109be2381d0a19483c
WalterRamos22 <wramosc4@miumg.edu.gt>
2024-11-02 07:53:58 -0600

    Update DatabaseConnection.java

--- Examen_Final/src/main/java/Examen_Final/DatabaseConnection.java
+++ Examen_Final/src/main/java/Examen_Final/DatabaseConnection.java
@@ -8,6 +8,16 @@ package Examen_Final;
  *
  * @author Walter Ramos
  */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
-    
+    private static final String URL = "jdbc:mysql://localhost:3306/zoologico"; // Cambia la URL según tu configuración
+    private static final String USER = "root";
+    private static final String PASSWORD = "administrador";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 }
